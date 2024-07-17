e80d61d85b0895ca1bbd4e11fc02851fdd3ebafd
BhargabRC <99333346+BhargabRC@users.noreply.github.com>
2024-07-17 23:15:16 +0530

    final commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+    'mongodb+srv://bhargabrc:Um1wBJTF6CfELz72@bookstoremern.lxjn6do.mongodb.net/?retryWrites=true&w=majority&appName=BookstoreMERN';
